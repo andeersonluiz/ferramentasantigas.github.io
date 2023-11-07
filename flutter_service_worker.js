7f19e104f846f55317ff01d818dea17d7588090a
Anderson Luiz Bernardes da Rocha <andeersonluiz1998@gmail.com>
2023-11-07 01:31:25 -0300

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,14 +65,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "6461499be2a3906ee126cc2963f6c179
 "ferramentasantigas/.git/refs/heads/main": "d847a8b7ceef49285d1a3eac39737343",
 "ferramentasantigas/.git/refs/remotes/origin/HEAD": "98b16e0b650190870f1b40bc8f4aec4e",
 "ferramentasantigas/index.html": "7fe3d4922323f9a387bc27cf030256ec",
-"/": "81f4bd9d20bc450aa8867e3ff7af3a09",
+"/": "d477a0b854d5d3ed001626f173e21211",
 "flutter.js": "6b515e434cea20006b3ef1726d2c8894",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "81f4bd9d20bc450aa8867e3ff7af3a09",
-"main.dart.js": "4bed82dbede98525b5089810f4856dd5",
+"index.html": "d477a0b854d5d3ed001626f173e21211",
+"main.dart.js": "1690dfe771011e3d0f9f1f258a8a37f5",
 "manifest.json": "27a4f882cc3dda1b1aab9851879a70c4",
 "version.json": "4a43403687e295c2c7b3cb07bbef31bc"};
 // The application shell files that are downloaded before a service worker can
